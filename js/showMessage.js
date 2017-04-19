2eaa46812b23133b5f2ce0513de2b4d9ac6afa1b
IFmiss <185098535@qq.com>
2017-04-20 00:00:56 +0800

    属性值类型的更改

--- js/showMessage.js
+++ js/showMessage.js
@@ -25,7 +25,7 @@
 				content:              "这是一个提示信息",       //提示内容
 				color:                "#fff",                   //文字颜色
 				top:            	  "80%",                	//bottom底部的位置    具体的数值 或者center  垂直居中
-				zIndex:               '1000001',                //层级
+				zIndex:               1000001,                	//层级
 				isCenter:   		  true, 					//是否垂直水平居中显示
 				closePrev: 			  true, 					//在打开下一个toast的时候立即关闭上一个toast
 		    }
